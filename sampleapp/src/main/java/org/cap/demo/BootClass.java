9e29f90975624e3009a18dfe10407dff539724ce
Abhibhosalecr7 <abhibhosalecr7@gmail.com>
2021-09-20 11:38:00 +0530

    array in show method

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -2,9 +2,18 @@ package org.cap.demo;
 
 public class BootClass {
 
-	public static void main(String[] args) {
-		System.out.println("Hello from STS");
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello! from Eclipse!");
+		show(); 
+		}
+	public static void show() {
+		int[] arr= {25,9,21,1};
+		for(int i=0;i<4;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		System.out.println("Show method here!");
 	}
 
 }
